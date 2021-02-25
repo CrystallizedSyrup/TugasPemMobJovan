f948541a6d19365d098c808b04082d4475846d4e
CrystallizedSyrup <jovanko5828@gmail.com>
2021-02-25 09:39:09 +0700

    Week04B

---
+++ Week04B_32025/app/src/main/java/id/ac/umn/week04b_32025/FirstFragment.java
@@ -0,0 +1,32 @@
+package id.ac.umn.week04b_32025;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import java.util.Objects;
+
+public class FirstFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        return view;
+    }
+
+
+
+}
